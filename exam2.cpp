faa9599da0c012ca4376445c334d7bf1ad24488c
Unknown <yuwadee.dd58@gmail.com>
2018-02-20 13:59:35 +0700

    สอบข้อ 2

--- exam2.cpp
+++ exam2.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
-int main() {
+int main()
+{
+    ofstream myfile ("std.txt");
+    if (myfile.is_open())
+    {
+        myfile << "yuwadee ponok.\n";
+        myfile.close();
+    }
 
-        return 0;
 }
+    else cout << "Unable to open file";
+
+    do
+{
+    cout << "Menu" << endl;
+    cout << "1.for add student." << endl;
+    cout << "2.list student." << endl;
+    cout << "3.quit." << endl;
+    cout << "Please enter menu : ";
+    cin >> cmd;
+
+    if(cmd == 1)
+    {
+        cout << "Enter std name : ";
+        cin >> name;
+        writeFile.open("std.txt", fstream::app);
+
+        if (writeFile.is_open())
+        {
+            writeFile << name << endl;
+            writeFile .close();
+        }
+        else
+        {
+            cout << "Unable to open file";
+        }
+    }
+    else if (cmd == 2)
+    {
+        readFile.open("std.txt");
+        if (readFile.is_open())
+        {
+            while (getline (readFile,line))
+            {
+                cout << line << endl ;
+            }
+            readFile.close();
+        }
+    }
+    else{
+        break;
+    }
+}
+    while(1);
+
+            return 0;
+        }
